225ed86a714f1e018582dc254f3da24d9fa5e0ab
Oleksandra Bershatska <oleksandra.bershatska@solarwinds.com>
2021-12-27 21:58:31 +0200

    Michael and Scott queue

---
+++ ParallelComputing_lab/MichaelScottQueue.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using ParallelComputing_lab.Node;
+
+namespace ParallelComputing_lab
+{
+    public class MichaelScottQueue<T>
+    {
+        public QueueNode<T> Top;
+        public QueueNode<T> Tail;
+
+        public MichaelScottQueue()
+        {
+            var node = new QueueNode<T>(default(T), null);
+            Top = node;
+            Tail = node;
+        }
+
+        public bool Push(T value)
+        {
+            var node = new QueueNode<T>(value, null);
+            while (true)
+            {
+                var tail = Tail;
+                if (tail != null && CompareAndSet(ref tail.Next, node, null))
+                {
+                    CompareAndSet(ref Tail, node, tail);
+                    return true;
+                }
+
+                CompareAndSet(ref Tail, tail.Next, tail);
+            }
+        }
+
+        public T Pop()
+        {
+            while (true)
+            {
+                var top = Top;
+                var tail = Tail;
+                var nextTop = top.Next;
+
+                if (top == tail)
+                {
+                    if (nextTop == null) throw new Exception();
+                    CompareAndSet(ref Tail, nextTop, tail);
+                }
+                else
+                {
+                    if (CompareAndSet(ref Top, nextTop, top)) return nextTop.Value;
+                }
+            }
+        }
+        
+        private bool CompareAndSet (ref QueueNode<T> location, QueueNode<T> value, QueueNode<T> comparand)
+        {
+            return comparand == Interlocked.CompareExchange(ref location, value, comparand);
+        }
+    }
+}
---
+++ ParallelComputing_lab/Node/QueueNode.cs
@@ -0,0 +1,14 @@
+﻿namespace ParallelComputing_lab.Node
+{
+    public class QueueNode<T>
+    {
+        public T Value { get; }
+        public QueueNode<T> Next;
+
+        public QueueNode(T value, QueueNode<T> next)
+        {
+            Value = value;
+            Next = next;
+        }
+    }
+}
